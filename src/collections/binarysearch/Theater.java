0a5455b31756b407cc327d11018a677258c3ee9d
DESKTOP-M451O6I\John <f40706@gmail.com>
2022-08-16 22:38:03 +0800

    1.新增Theater Binary註解

--- src/collections/binarysearch/Theater.java
+++ src/collections/binarysearch/Theater.java
@@ -1,22 +1,26 @@
 package collections.binarysearch;
 
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Theater {
     private final List<Seat> list = new ArrayList<>();
+
     public Theater() {
         addAll();
     }
 
     public Seat binarySearch(int index) {
         Seat seat = new Seat(index, getSeatName(index));
+        /*
+          Seat必須實作Comparable<Seat>，否則會拋異常
+          第一格是List
+          第二格是Item
+          第三格如果是null，使用自然排序
+          */
         int searchSeat = Collections.binarySearch(list, seat, null);
-        if(searchSeat >= 0) {
+        if (searchSeat >= 0) {
             return list.get(searchSeat);
         } else {
             return null;
@@ -24,7 +28,7 @@ public class Theater {
     }
 
     private void addAll() {
-        for(int i=0;i<10;i++) {
+        for (int i = 0; i < 10; i++) {
             String name = getSeatName(i);
             Seat seat = new Seat(i, name);
             list.add(seat);
